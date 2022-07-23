7d607fb0b0c33bc643c7a4fa245806046904c1e4
Mendy2050 <107521996+Mendy2050@users.noreply.github.com>
2022-07-23 12:45:12 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let countHome = 0
+
+function incrementHome1() {
+    countHome = countHome + 1
+    document.getElementById("home-num-record").textContent = countHome
+}
+
+
+function incrementHome2() {
+    countHome = countHome + 2
+    document.getElementById("home-num-record").textContent = countHome
+}
+
+function incrementHome3() {
+    countHome = countHome + 3
+    document.getElementById("home-num-record").textContent = countHome
+}
+
+let countGuest = 0
+
+function incrementGuest1() {
+    countGuest = countGuest + 1
+    document.getElementById("guest-num-record").textContent = countGuest
+}
+
+
+function incrementGuest2() {
+    countGuest = countGuest + 2
+    document.getElementById("guest-num-record").textContent = countGuest
+}
+
+function incrementGuest3() {
+    countGuest = countGuest + 3
+    document.getElementById("guest-num-record").textContent = countGuest
+}
